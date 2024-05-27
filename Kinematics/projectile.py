96b66ff449f0968cc45491d96e34729760db3fcd
Tejas Rajegowda B <140500062+tejasrajegowda@users.noreply.github.com>
2024-05-27 12:39:27 +0530

    Update projectile.py

--- Kinematics/projectile.py
+++ Kinematics/projectile.py
@@ -2,8 +2,6 @@ import math
 import random
 import pygame
 
-from projfunc import *
-
 pygame.init()
 SCREEN = WIDTH, HEIGHT = 1500, 700
 
@@ -37,6 +35,34 @@ origin = (20, 340)
 radius = 250
 
 g = 9.8
+
+def toRadian(theta):
+    return theta * math.pi / 180
+
+def toDegrees(theta):
+    return theta * 180 / math.pi
+
+def getGradient(p1, p2):
+    if p1[0] == p2[0]:
+        m = toRadian(90)
+    else:
+        m = (p2[1] - p1[1]) / (p2[0] - p1[0])
+    return m
+
+def getAngleFromGradient(gradient):
+    return math.atan(gradient)
+
+def getAngle(pos, origin):
+    m = getGradient(pos, origin)
+    thetaRad = getAngleFromGradient(m)
+    theta = round(toDegrees(thetaRad), 2)
+    return theta
+
+def getPosOnCircumeference(theta, origin):
+    theta = toRadian(theta)
+    x = origin[0] + radius * math.cos(theta)
+    y = origin[1] + radius * math.sin(theta)
+    return (x, y)
 
 class Projectile(pygame.sprite.Sprite):
     def __init__(self, u, theta):
